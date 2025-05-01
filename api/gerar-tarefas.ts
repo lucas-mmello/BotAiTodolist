42a3778caf0910fc5eaefe937caf1a7e7e4e4abb
lucas mello <lucasmello03@hotmail.com>
2025-05-01 18:47:22 -0300

    Ajustes de cors

--- api/gerar-tarefas.ts
+++ api/gerar-tarefas.ts
@@ -8,9 +8,6 @@ const openai = new OpenAI({
 console.log(process.env.OPENAI_API_KEY);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
